1bfb06e5847c9cebe68043e40a80c86710a3cec5
Kim <kim.yang@waveo.com>
2019-09-30 13:48:28 +0800

    Add components & styles

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,36 @@
+import './SeasonDisplay.css';
+import React from 'react';
+
+const seasonConfig = {
+  summer: { 
+    text: 'Let\'s hit the beach',
+    iconName: 'massive sun icon',
+  },
+  winter: {
+    text: 'Blur, it\'s cold',
+    iconName: 'massive snowflake icon',
+  },
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+}
+
+const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const {text, iconName} = seasonConfig[season]
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left ${iconName}`}></i>
+      <h1>{text}</h1>
+      <i className={`icon-right ${iconName}`}></i>
+    </div>
+  );
+}
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+import useLocation from './useLocation';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  const [ lat, errorMessage ] = useLocation();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else {
+    content = <Spinner message="Please accept location request"/>;
+  }
+
+  return (
+    <div className="border red">
+      {content}
+    </div>
+  )
+}
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { lat: null, errorMsg: '' };
+//   }
+
+//   componentDidMount() {
+//     window.navigator.geolocation.getCurrentPosition(
+//       position => {
+//           this.setState({ lat: position.coords.latitude });
+//       },
+//       err => {
+//         this.setState({ errorMsg: err.message});
+//       },
+//     );
+//   }
+
+//   renderContent() {
+//     if (this.state.errorMsg && !this.state.lat) {
+//       return <div>Error: {this.state.errorMsg}</div>
+//     }
+
+//     if (!this.state.errorMsg && this.state.lat) {
+//       return <SeasonDisplay lat={this.state.lat} />
+//     }
+
+//     return <Spinner message="Please accept location request"/>
+//   }
+
+//   render() {
+//     return (
+//       <div className="border red">
+//         {this.renderContent()}
+//       </div>
+//     )
+//   }
+// }
+ReactDOM.render(<App />, document.querySelector('#root'));
